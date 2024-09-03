8d95f1bf708cbaac416f4eaca2bbb6c39938ae27
NotoriuxX <manuelmeryz@gmail.com>
2024-09-02 22:28:43 -0400

    Configuración de Tailwind y actualización de archivos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Incluye todos los archivos JS, JSX, TS y TSX dentro de la carpeta src
+    './public/index.html', // Incluye también el archivo index.html si estás utilizando Tailwind en él
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
